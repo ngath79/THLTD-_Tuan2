fccd6055343c7b71a03d503ebf6b0fbe629860b3
ngath79 <maithinga7902@gmail.com>
2022-08-30 10:30:21 +0700

    first commit

---
+++ app/src/main/java/com/google/th_tuan2_bmi/MainActivity.java
@@ -0,0 +1,37 @@
+package com.google.th_tuan2_bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editcannang;
+    EditText editchieucao;
+    Button ketqua;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editcannang.findViewById(R.id.editcannang);
+        editchieucao.findViewById(R.id.editchieucao);
+        ketqua.findViewById(R.id.ketqua);
+
+        ketqua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n = Double.parseDouble(editcannang.getText().toString());
+                double c = Double.parseDouble(editchieucao.getText().toString());
+                Intent intent = new Intent(MainActivity.this,ketqua.class);
+                intent.putExtra("cao", c);
+                intent.putExtra("nang",n);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/google/th_tuan2_bmi/ketqua.java
@@ -0,0 +1,21 @@
+package com.google.th_tuan2_bmi;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ketqua extends AppCompatActivity {
+    Button backmain;
+    TextView textviewketqua;
+    TextView textviewloikhuyen;
+
+    protected void onCreate Bundle saveInstanceState) {
+    super.onCreate();
+    setContentView(R.layout.activity_ketqua);
+
+    backmain = findViewById()
+    }
+
+}
